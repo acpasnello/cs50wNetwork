5051304f3a63c0f91fff41dec467b0a323fdf84d
acpasnello <anthony.pasnello@gmail.com>
2022-10-06 11:15:05 -0400

    Create Post, Like, and Follow models

--- network/models.py
+++ network/models.py
@@ -4,3 +4,19 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    poster = models.ForeignKey(User, on_delete=models.CASCADE, related_name="MyPosts")
+    content = models.TextField( )
+    posted = models.DateTimeField(auto_now_add=True)
+    lastmodified = models.DateTimeField(auto_now=True)
+    likecount = models.IntegerField()
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="MyLikes")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+class Follow(models.Model):
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name="")
+    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name="")
