757fff66c8e3a7023b04ea8a523eb02b257ed3e1
acpasnello <anthony.pasnello@gmail.com>
2022-10-11 14:08:34 -0400

    Add JS file. Add CSS styling to new post form. Update newpost form.

--- network/forms.py
+++ network/forms.py
@@ -6,3 +6,9 @@ class PostForm(ModelForm):
     class Meta:
         model = Post
         fields = ['content']
+        labels = {
+            'content': _('New Post')
+        }
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['content'].widget.attrs.update({'id': 'newposttext'})
